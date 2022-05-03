16b380f6996e627af33280fad842e27a6e622efd
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-03 15:29:13 +0200

    Added header site

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,28 @@
 <template>
     <header>
-        test
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <img src="@/assets/img/dc-logo.png" alt="logo">
+                </div>
+                <div class="col-8">
+                     <nav>
+                        <ul>
+                            <li>characters</li>
+                            <li class="active">comics</li>
+                            <li>movies</li>
+                            <li>tv</li>
+                            <li>games</li>
+                            <li>collectibles</li>
+                            <li>videos</li>
+                            <li>fans</li>
+                            <li>news</li>
+                            <li>shop</li>
+                        </ul>
+                     </nav>
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -11,5 +33,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+    header{
+       .container{
+           margin: auto;
+           width: 1300px;
+       }
+
+       .row{
+           display: flex;  
+           align-items: center;
+           justify-content: space-around;
+       }
+       img{
+           width: 70px;
+       }
+    }
+
+    nav{
+        ul{
+            list-style: none;
+
+            li{
+                display: inline-block;
+                text-transform: uppercase;
+                padding: 32px 10px;
+                font-size: 12px;
+                font-weight: 700;
+            }
+        }
+    }
+
+    .active {
+    color: blue;
+    position: relative;
+}
+.active:before {
+  content: "";
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  width: 50%; 
+  height: 1px;
+  transform: translate(-50%,-50%);
+  border-bottom: 2px solid blue;
+}
    
 </style>
